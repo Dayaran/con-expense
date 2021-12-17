cd67da42245e30b77e9be6611a397a85a191e77e
Dayaran <lanplore@gmail.com>
2021-12-17 16:37:27 +0530

    Updates

---
+++ precache-manifest.252a06c687e9ae2069031b7ad835f314.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eea58eec88165fd71ba3",
+    "url": "/con-expense/static/css/main.3cf018ee.chunk.css"
+  },
+  {
+    "revision": "eea58eec88165fd71ba3",
+    "url": "/con-expense/static/js/main.eea58eec.chunk.js"
+  },
+  {
+    "revision": "80eb21c96a0a6d29c881",
+    "url": "/con-expense/static/js/1.80eb21c9.chunk.js"
+  },
+  {
+    "revision": "c2eb5dea4e0c29d76507",
+    "url": "/con-expense/static/js/runtime~main.c2eb5dea.js"
+  },
+  {
+    "revision": "34cf53375f840ece721fc985de40d881",
+    "url": "/con-expense/static/media/loader.34cf5337.gif"
+  },
+  {
+    "revision": "d0f00eb0775b509a5064714defb52cfb",
+    "url": "/con-expense/index.html"
+  }
+];
